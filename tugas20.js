db3c9e63480cd3206f2750b5d53144860644b932
s-prianto <prianto2796@gmail.com>
2020-02-12 06:47:00 +0700

    Tugas 20 JS

---
+++ tugas20.js
@@ -0,0 +1,7 @@
+function Match(){
+	var x = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+	console.log(x.match("are"));
+	console.log(x.match("words"));
+}
+
+Match()
